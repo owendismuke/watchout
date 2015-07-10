a86705d9f074c160a3d5078091485f12081b265e
Owen Dismuke <owen.dismuke@gmail.com>
2015-07-10 16:05:44 -0700

    We have movement!!!!
    Using .transition()

--- watchout.js
+++ watchout.js
@@ -12,18 +12,29 @@ var svg = d3.select("body").append("svg")
 // update is a function to move the enemies around the board
 // future - will need to perform collision detection during the move
 var update = function() {
+  // temporary way to generate data for enemy circles, will refactor later?
+  // currently only purpose is to determine number of enemy dots
+  // include a slider or difficulty setting on index.html, use that to determine 
+  // number of enemies?
   var tempArray = [];
   for (var i = 0; i < 20; i++) {
     tempArray.push(i);
   }
   // define an enemy with predetermined radius, and random center based on size of canvas
   var enemy = svg.selectAll("circle").data(tempArray);
-
+  // selects all previously existing enemies, assigns new (x,y) coords
+  enemy.transition() 
+    .attr("cx", function() { return Math.random() * (width - radius) + radius; })
+    .attr("cy", function() { return Math.random() * (height - radius) + radius; })
+    .duration(1000);
+  // creates new enemies, assigns initial (x,y) coords
   enemy.enter().append("circle")
     .attr("r", radius)
     .attr("cx", function() { return Math.random() * (width - radius) + radius; })
     .attr("cy", function() { return Math.random() * (height - radius) + radius; });
+  // currently unused, but can add removal functionality if difficulty selector is implemented
+  enemy.exit().remove();
 
 };
-
 update();
+setInterval(update, 1000);
