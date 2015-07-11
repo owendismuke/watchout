52763f0949935d7283e0c255d7cb7a603ac316e0
Owen Dismuke <owen.dismuke@gmail.com>
2015-07-11 12:50:17 -0700

    Collision only detects one collision per movement. Scoring implemented. Enemies are now svg shuriken. Enemies rotate with css keyframes.

--- watchout.js
+++ watchout.js
@@ -4,6 +4,10 @@ var width = 800;
 var height = 600;
 var radius = 15;
 
+var highScore = 0;
+var currentScore = 0;
+var collisions = 0;
+
 
 var svg = d3.select("body").append("svg")
   // future idea - make canvas size responsive to size of browser window
@@ -32,12 +36,21 @@ var player = svg.selectAll(".player").data([{x: width/2, y: height/2}])
               .style("fill", "red")
               .call(drag);
 
+var increaseScore = function() {
+  currentScore++;
+  if (currentScore > highScore) {
+    highScore = currentScore;
+  }
+  d3.select(".current span").text(currentScore);
+  d3.select(".high span").text(highScore);
+};
+
 var onCollision = function(){
-  var collsSpan = d3.select(".collisions span");
-  var collisions = Number(collsSpan.text());
-
-  collsSpan.text(++collisions);
-
+  // increment collisions
+  d3.select(".collisions span").text(++collisions);
+  // reset current score to 0
+  currentScore = 0;
+  d3.select(".current span").text(currentScore);
 };
 
 // checkCollision somehow needs to check the distance between an enemy and the player, and if there
@@ -45,14 +58,17 @@ var onCollision = function(){
 var checkCollision = function(enemy, collidedCallback) { 
   //grab current enemy x,y, and r
   //grab player x,y, r
-  var radiusSum = parseFloat(enemy.attr("r")) + parseFloat(player.attr("r"));
-  var xDiff = parseFloat(enemy.attr("cx")) - parseFloat(player.attr("cx"));
-  var yDiff = parseFloat(enemy.attr("cy")) - parseFloat(player.attr("cy"));
+  var radiusSum = 15 + parseFloat(player.attr("r"));
+  var xDiff = parseFloat(enemy.attr("x")) - parseFloat(player.attr("cx"));
+  var yDiff = parseFloat(enemy.attr("y")) - parseFloat(player.attr("cy"));
 
   var separation = Math.sqrt( Math.pow(xDiff, 2) + Math.pow(yDiff, 2) );
 
   //check separation with formula to at least sum of radii
   if (separation < radiusSum) {
+    enemy.data("collided", function(d) {
+      d.collided = true;
+    });
     collidedCallback();
   }
   //if collided
@@ -68,31 +84,20 @@ var checkCollision = function(enemy, collidedCallback) {
 // and then calculates a future (x,y) for next tick, and sets that as the new position of enemy
 var tweenWithCollisionDetection = function(endData) {
   var enemy = d3.select(this);
-  
-  var startPos = {
-    x: parseFloat(enemy.attr("cx")),
-    y: parseFloat(enemy.attr("cy"))
-  };
-
-  var endPos = {
-    x: endData.x,
-    y: endData.y
-  };
-
 
   return function(t) {
-    checkCollision(enemy, onCollision);
+    if (!endData.collided) {
+      checkCollision(enemy, onCollision);
+    }
 
-    var enemyNextPos = {
-      x: startPos.x + (endPos.x - startPos.x)*t,
-      y: startPos.y + (endPos.y - startPos.y)*t
-    };
-
-    enemy.attr("cx", enemyNextPos.x)
-         .attr("cy", enemyNextPos.y);
+    if (t === 1) {
+      if (!endData.collided) {
+        // run score update function
+        increaseScore();
+      }
+      endData.collided = false;
+    }
   };
-
-
 };
 
 // we will also eventually want functions to keep track of and update current score, high score, and deaths
@@ -102,26 +107,37 @@ var tweenWithCollisionDetection = function(endData) {
 var update = function() {
 
   // define an enemy with predetermined radius, and random center based on size of canvas
-  var enemy = svg.selectAll("circle.enemy")
-  .data(d3.range(16).map(function() { return {x: Math.random() * (width - radius) + radius, y: Math.random() * (height - radius) + radius}; }));
+  var enemy = svg.selectAll("svg.enemy")
+  .data(d3.range(16).map(function() { return {x: Math.random() * (width - radius) + radius, y: Math.random() * (height - radius) + radius, collided: false}; }));
   // selects all previously existing enemies, assigns new (x,y) coords
   enemy.transition() 
-    .attr("cx", function(d) {
+    .attr("x", function(d) {
                   d.x = Math.random() * (width - radius) + radius; 
                   return d.x;
                 })
-    .attr("cy", function(d) { 
+    .attr("y", function(d) { 
                   d.y = Math.random() * (height - radius) + radius; 
                   return d.y;
                 })
     .duration(1000)
     .tween("collisions", tweenWithCollisionDetection); //Implement tween
   // creates new enemies, assigns initial (x,y) coords
-  enemy.enter().append("circle")
-    .attr("r", radius)
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
-    .attr("class", "enemy");
+  enemy.enter().append("svg")
+    .attr("x", function(d) { return d.x; })
+    .attr("y", function(d) { return d.y; })
+    .attr("class", "enemy")
+    .each(function() {
+      var enemySVG = d3.select(this);
+      enemySVG.append("path")
+        .attr("class", "rotate")
+        .attr("d", "m 15 0 l 5 10 l 10 5 l -10 5 l -5 10 l -5 -10 l -10 -5 l 10 -5 l 5 -10");
+      enemySVG.append("circle")
+        .attr("cx", 15)
+        .attr("cy", 15)
+        .attr("r", 2)
+        .attr("fill", "white");
+    });
+    
   // currently unused, but can add removal functionality if difficulty selector is implemented
   enemy.exit().remove();
 
